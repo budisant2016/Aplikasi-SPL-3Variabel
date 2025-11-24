53e64c1b5d58d39f5f4aa1bed9f9cdf91ab45c71
budisant2016 <budisolution@gmail.com>
2025-11-24 12:10:17 +0700

    Add number input for variable A1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("Aplikasi Web Solusi SPL 3 Variabel dg Eliminasi Gauss")
 st.write(
     "Isikan masing-masing indeks variabel x1-x3."
 )
+a1 = st.number_input("Masukkan indeks variabel A1:", min_value=0, max_value=100)
