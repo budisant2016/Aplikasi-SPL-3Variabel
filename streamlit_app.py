63330b66a53e10307a5a76a67f60c311600fcce0
budisant2016 <budisolution@gmail.com>
2025-11-24 11:53:03 +0700

    Update app title to reflect new project purpose

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Aplikasi Web Solusi SPL 3 Variabel dg Eliminasi Gauss")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
